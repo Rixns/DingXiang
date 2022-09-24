ed065c9276e47f07a0b13fb137141834aa6ce3bc
Rixns <793315195@qq.com>
2022-09-24 22:33:47 +0800

    new

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,9 @@
 
 const base = {
     host: "http://api.tianapi.com",
-    CovInfo: "/ncov/index?key=af81238676922e5c305c6b1ad755d5fc"
+    baseUrl: "https://rijb.api.storeapi.net",
+    CovInfo: "/ncov/index?key=af81238676922e5c305c6b1ad755d5fc",
+    chinaData: "/api/94/219?format=json&appid=21779&sign=8761e5e168053d483769c51673b4c8b9"
 }
 
 export default base
--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,9 @@ import base from "./base";
 const api = {
     getCovInfo() {
         return axios.get(base.host + base.CovInfo)
+    },
+    getChinaData() {
+        return axios.get(base.baseUrl + base.chinaData)
     }
 }
 
